a3993a56fa804bc7a3c28eda75d489e5f2a5c8d8
Adedapo Ojo <ojoadedapo47@gmail.com>
2026-05-25 10:26:33 -0600

    debug: test minimal HTTP server on HF

---
+++ apps/web/start.js
@@ -0,0 +1,25 @@
+const { spawn } = require('child_process')
+
+const port = process.env.PORT || '7860'
+const hostname = process.env.HOSTNAME || '0.0.0.0'
+
+const server = spawn('npx', ['next', 'start', '-p', port, '-H', hostname], {
+  stdio: ['inherit', 'pipe', 'pipe'],
+  env: { ...process.env },
+})
+
+server.stdout.on('data', (d) => process.stdout.write(d))
+server.stderr.on('data', (d) => process.stderr.write(d))
+
+server.on('error', (err) => {
+  console.error('Failed to start:', err)
+  process.exit(1)
+})
+
+server.on('exit', (code) => {
+  console.error('Server exited with code:', code)
+  process.exit(code || 1)
+})
+
+process.on('SIGTERM', () => server.kill())
+process.on('SIGINT', () => server.kill())
